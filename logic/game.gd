8dc6ce09188243bcdf495f5af4572bf8fea49530
PhilipU21 <philipuriarte@gmail.com>
2022-07-30 16:58:23 +0800

    Add onready var history_rows

--- logic/game.gd
+++ logic/game.gd
@@ -4,5 +4,7 @@ extends Control
 const InputResponse = preload("res://scenes/input_response.tscn")
 
 
+onready var history_rows = $Background/MarginContainer/Rows/GameInfo/ScrollContainer/HistoryRows
+
 func _on_Input_text_entered(new_text: String) -> void:
 	pass # Replace with function body.
