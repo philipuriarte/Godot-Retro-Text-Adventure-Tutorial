a4a6efdeb315453c96ac6588d972ae8edfd15dd8
PhilipU21 <philipuriarte@gmail.com>
2022-07-30 17:03:25 +0800

    Add code to game.gd to add input_response scene

--- logic/game.gd
+++ logic/game.gd
@@ -7,4 +7,5 @@ const InputResponse = preload("res://scenes/input_response.tscn")
 onready var history_rows = $Background/MarginContainer/Rows/GameInfo/ScrollContainer/HistoryRows
 
 func _on_Input_text_entered(new_text: String) -> void:
-	pass # Replace with function body.
+	var input_response = InputResponse.instance()
+	history_rows.add_child(input_response)
