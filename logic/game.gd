07d5562c9a8a9e75cdbe9348d57b8c5882727b78
PhilipU21 <philipuriarte@gmail.com>
2022-07-30 16:09:00 +0800

    Add clear function

--- logic/game.gd
+++ logic/game.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _ready() -> void:
 	pass
+
+
+func _on_Input_text_entered(new_text: String) -> void:
+	pass # Replace with function body.
--- logic/input.gd
+++ logic/input.gd
@@ -2,3 +2,7 @@ extends LineEdit
 
 func _ready() -> void:
 	grab_focus()
+
+
+func _on_Input_text_entered(new_text: String) -> void:
+	clear()
