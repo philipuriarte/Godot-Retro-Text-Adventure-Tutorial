965ef2e8b637ad1c009a1d6df08e05be80525593
PhilipU21 <philipuriarte@gmail.com>
2022-08-01 13:51:19 +0800

    Slight changes to game.gd

--- logic/game.gd
+++ logic/game.gd
@@ -6,19 +6,20 @@ const InputResponse = preload("res://scenes/input_response.tscn")
 
 # Contains the HistoryRows node
 onready var history_rows = $Background/MarginContainer/Rows/GameInfo/ScrollContainer/HistoryRows
-onready var scroll_container = $Background/MarginContainer/Rows/GameInfo/ScrollContainer
-onready var scrollbar = scroll_container.get_v_scrollbar()
+onready var scroll_con = $Background/MarginContainer/Rows/GameInfo/ScrollContainer
+onready var scrollbar = scroll_con.get_v_scrollbar()
 
 
 func _ready() -> void:
 	scrollbar.connect("changed", self, "handle_scrollbar_changed")
 
 
+# Will execute if value of ScrollContainer changes
 func handle_scrollbar_changed():
-	scroll_container.scroll_vertical = scrollbar.max_value
+	scroll_con.scroll_vertical = scrollbar.max_value
 
 
-# Wil execute when user presses enter
+# Will execute when user presses enter
 func _on_Input_text_entered(new_text: String) -> void:
 	var input_response = InputResponse.instance()
 	input_response.set_text(new_text, "Response")
