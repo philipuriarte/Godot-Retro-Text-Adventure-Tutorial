1d8c43a479d745fd9e90acff7d703960c46f10f0
PhilipU21 <philipuriarte@gmail.com>
2022-08-01 13:39:58 +0800

    Add onready variables for auto-scroll functionality

--- logic/game.gd
+++ logic/game.gd
@@ -6,6 +6,8 @@ const InputResponse = preload("res://scenes/input_response.tscn")
 
 # Contains the HistoryRows node
 onready var history_rows = $Background/MarginContainer/Rows/GameInfo/ScrollContainer/HistoryRows
+onready var scroll_container = $Background/MarginContainer/Rows/GameInfo/ScrollContainer
+onready var scrollbar = scroll_container.get_v_scrollbar()
 
 
 # Wil execute when user presses enter
