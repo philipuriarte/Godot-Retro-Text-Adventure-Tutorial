6229f419d7c53dfd2786bd8621d46d5c9de7e9b3
PhilipU21 <philipuriarte@gmail.com>
2022-08-07 03:56:37 +0800

    Update func process_command

--- logic/command_processor.gd
+++ logic/command_processor.gd
@@ -1,4 +1,10 @@
 extends Node
 
 func process_command(input: String):
-	pass
+	var words = input.split(" ", false)
+	var first_word = words[0]
+	var second_word = words[1]
+	
+	match first_word:
+		"go":
+			pass
