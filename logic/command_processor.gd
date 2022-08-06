aa1b0f411bb1f4fc0da558235dfc824a55d16995
PhilipU21 <philipuriarte@gmail.com>
2022-08-07 04:04:30 +0800

    Update var second_word value and add condition to set it

--- logic/command_processor.gd
+++ logic/command_processor.gd
@@ -3,7 +3,10 @@ extends Node
 func process_command(input: String):
 	var words = input.split(" ", false)
 	var first_word = words[0].to_lower()
-	var second_word = words[1].to_lower()
+	var second_word = ""
+	
+	if words.size() > 1:
+		second_word = words[1].to_lower()
 	
 	match first_word:
 		"go":
