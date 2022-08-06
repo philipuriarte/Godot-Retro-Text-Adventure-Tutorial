d8324447e6b81ce4aff2a795c9f661ba79f56503
PhilipU21 <philipuriarte@gmail.com>
2022-08-07 04:07:54 +0800

    Update func process_command to return string var

--- logic/command_processor.gd
+++ logic/command_processor.gd
@@ -1,6 +1,6 @@
 extends Node
 
-func process_command(input: String):
+func process_command(input: String) -> String:
 	var words = input.split(" ", false)
 	var first_word = words[0].to_lower()
 	var second_word = ""
@@ -10,7 +10,7 @@ func process_command(input: String):
 	
 	match first_word:
 		"go":
-			go(second_word)
+			return go(second_word)
 
 
 func go(second_word: String):
